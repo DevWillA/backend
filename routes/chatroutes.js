212d65be660c42be2a4c347633cd7c17283a615f
WillDevLenovo <williamjavi.140@gmail.com>
2025-03-28 15:47:39 -0500

    Yes

--- routes/chatroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addChat, getChat } = require('../controllers/chatcontroller');
-const router = express.Router();
-
-router.post('/add', addChat);
-router.get('/', getChat);
-
-module.exports = router;
